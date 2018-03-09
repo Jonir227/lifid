800cd66e4e2b7b22c63c825053fe956e02b2f9ba
jonir227 <bjbj6363@gmail.com>
2018-03-09 21:43:34 +0900

    texteditor made with quill updated

--- src/components/LeftBar/LeftBar.jsx
+++ src/components/LeftBar/LeftBar.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ClassNames from 'classnames/bind';
 import { Card } from '@blueprintjs/core';
+import _ from 'lodash';
 import styles from './LeftBar.scss';
 
 const cx = ClassNames.bind(styles);
 
 const LeftBar = ({ sections, indexFunction }) => {
   return (
-    <div className={cx('left-bar')}>
-      <Card >
-        <strong>Table Of Content</strong>
-        <div>
-        </div>
-      </Card>
-    </div>
+    <Card className={cx('left-bar')}>
+      <strong>Table Of Content</strong>
+      <div className={cx('sections')}>
+        {
+          _.map(sections, (section) => {
+            if (section.length >= 3) {
+              console.log(section);
+              return <div className={cx('section-item')}>{section.substring(2)}</div>;
+            }
+          })
+        }
+      </div>
+    </Card>
   );
 };
 
--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -1,65 +1,64 @@
 import React from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
-import 'draft-js/dist/Draft.css';
-import { Card, ButtonGroup, Button } from '@blueprintjs/core';
+import { Card } from '@blueprintjs/core';
+import ReactQuill from 'react-quill';
 import ClassNames from 'classnames/bind';
+import _ from 'lodash';
 import { LeftBar } from 'components';
 import styles from './TextEditor.scss';
+import './quillTheme.css';
 
 const cx = ClassNames.bind(styles);
 
 class TextEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.quillRef = null;
+    this.reactQuillRef = null;
+  }
+
   state = {
-    editorState: EditorState.createEmpty(),
+    /*
+    text: editor의 내용
+    contentArr: editor의 내용을 줄로 쪼갠 것.
+    sections: ##[something] 형태로 작성된 것. Table Of Content에 들어간다.
+    */
+    text: '',
+    contentArr: [],
+    sections: [],
   }
 
-  onChange = (editorState) => {
-    console.log(editorState);
-    this.setState({ editorState });
+  onChange = (content, delta, source, editor) => {
+    this.setState({ text: content });
+    const ctnt = editor.getText();
+    if (delta.ops[1] !== undefined
+        && (delta.ops[1].insert === '\n'
+        || delta.ops[1].length > 1
+        || delta.ops[1].delete === 1)) {
+      const ctntArr = ctnt.slice(0, ctnt.length - 2).split('\n');
+      const sec = _.filter(ctntArr, str => str.startsWith('##'));
+      this.setState({
+        contentArr: ctntArr,
+        sections: sec,
+      });
+    }
   }
-
-  editorFocus = () => {
-    this.editor.focus();
-  }
-
-  _RichUtilCickFunc = styleMutator =>
-    () => { this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, styleMutator)); };
 
   render() {
     return (
       <div className={cx('editor')}>
         <Card className={cx('card-wrapper')}>
-          <LeftBar className={cx('left-bar')}/>
+          <LeftBar sections={this.state.sections} />
           <div className={cx('text-wrapper')}>
             <div className={cx('today-novel')}>
               &quot; 항구의 하늘은 방송이 끝난 텔레비전 색이였다 &quot;
             </div>
-            <div className={cx('editor-wrapper')} role="presentation" onClick={this.editorFocus} onKeyPress={this.focus}>
-              <div className={cx('tool-box')}>
-                <ButtonGroup minimal>
-                  <Button icon="header-one" />
-                  <Button icon="header-two" />
-                  <div className={cx('menu-divider')} />
-                  <Button icon="bold" onClick={this._RichUtilCickFunc('BOLD')} />
-                  <Button icon="italic" />
-                  <Button icon="underline" />
-                  <Button icon="strikethrough" />
-                  <div className={cx('menu-divider')} />
-                  <Button icon="align-left" />
-                  <Button icon="align-center" />
-                  <Button icon="align-right" />
-                  <Button icon="align-justfy" />
-                </ButtonGroup>
-              </div>
-              <div className={cx('text-write-area')}>
-                <Editor
-                  ref={(ref) => { this.editor = ref; }}
-                  editorState={this.state.editorState}
-                  onChange={this.onChange}
-                  placeholder="this area can be edited"
-                />
-              </div>
-            </div>
+            <ReactQuill
+              className={cx('text-write-area')}
+              value={this.state.text}
+              placeholder="start your dream"
+              onChange={this.onChange}
+            />
+            <div className={cx('insight-line')} >line count: {this.state.contentArr.length}&nbsp;&nbsp;&nbsp;word conut: {_.split(this.state.text, ' ').length}</div>
           </div>
         </Card>
       </div>
