c1860a4656df91f2d3bbc29648d405f12746dd38
Seungwoo Lee <starhn87@gmail.com>
2018-05-07 22:57:20 +0900

    today-novel api modified & deleted all admin api...

--- server/api/today-novel/index.js
+++ server/api/today-novel/index.js
@@ -4,6 +4,11 @@ const authMiddleWare = require('../../middleware/auth');
 
 router.use('/new', authMiddleWare);
 router.post('/new', controller.post);
+router.use('/now', authMiddleWare);
 router.get('/now', controller.now);
+router.use('/modify', authMiddleWare);
+router.put('/modify', controller.modify);
+router.use('/remove', authMiddleWare);
+router.delete('/remove', controller.remove);
 
 module.exports = router;
--- server/api/today-novel/today-novel.controller.js
+++ server/api/today-novel/today-novel.controller.js
@@ -2,30 +2,25 @@ const TodayNovel = require('../../models/todayNovel');
 
 // GET /api/today-novel/now
 exports.now = (req, res) => {
-  TodayNovel.find().sort({ dueDate: -1 }).limit(1)
-    .then((data) => {
-      const {
-        name,
-        author,
-        quotation,
-        dueDate,
-      } = data[0];
-
-      res.json({
-        name,
-        author,
-        quotation,
-        dueDate,
-      });
-    })
-    .catch((err) => {
-      res.status(403).json({
-        error: err,
-      });
+  if (!req.decoded.admin) {
+    res.status(403).json({
+      success: false,
+      message: 'you are not admin',
     });
+  } else {
+    TodayNovel.find().sort({ dueDate: -1 })
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(403).json({
+          error: err,
+        });
+      });
+  }
 };
 
-// POST /api/today-novel
+// POST /api/today-novel/new
 exports.post = (req, res) => {
   if (!req.decoded.admin) {
     res.status(403).json({
@@ -51,6 +46,67 @@ exports.post = (req, res) => {
         res.status(403).json({
           success: false,
           message: result,
+        });
+      });
+  }
+};
+
+// PUT api.today-novel/modify
+exports.modify = (req, res) => {
+  if (!req.decoded.admin) {
+    res.status(403).json({
+      success: false,
+      message: 'you are not admin',
+    });
+  } else {
+    const {
+      author,
+      name,
+      quotation,
+      dueDate,
+    } = req.body;
+
+    TodayNovel.findOneAndUpdate({ dueDate }, {
+      author, name, quotation,
+    })
+      .then((result) => {
+        console.log(result);
+
+        res.json({
+          success: true,
+        });
+      })
+      .catch((err) => {
+        res.status(403).json({
+          success: false,
+          error: err,
+        });
+      });
+  }
+};
+
+// DELETE /api/today-novel/remove
+exports.remove = (req, res) => {
+  if (!req.decoded.admin) {
+    res.status(403).json({
+      success: false,
+      message: 'you are not admin',
+    });
+  } else {
+    const {
+      dueDate,
+    } = req.body;
+    TodayNovel.deleteOne({ dueDate })
+      .then((result) => {
+        console.log(result);
+        res.json({
+          success: true,
+        });
+      })
+      .catch((err) => {
+        res.status(403).json({
+          success: false,
+          error: err,
         });
       });
   }
